b9b6ebf2a26ebda1aee48ce946b2a0a5bd588a85
Rishab Raj <rr0948555@gmail.com>
2026-03-02 13:52:25 +0530

    Routing

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterLink, RouterOutlet, RouterModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
