948185042943ebb2a0175f646cbc78de2d5f3e3b
Rishab Raj <rr0948555@gmail.com>
2026-03-03 09:48:27 +0530

    Greeting dynamic route

--- src/app/greeting/greeting.ts
+++ src/app/greeting/greeting.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-greeting',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './greeting.html',
   styleUrl: './greeting.css',
 })
-export class Greeting {
+export class Greeting implements OnInit{
+     userName: string = '';
 
+     constructor(private route: ActivatedRoute){}
+
+     ngOnInit(): void {
+          this.userName = this.route.snapshot.paramMap.get('name')!;
+     }
 }
