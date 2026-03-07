7a2f4a94cedfb32abe5352a4852ff3b9cd8095e4
Rishab Raj <rr0948555@gmail.com>
2026-03-07 07:45:27 +0530

    Cookie

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { CookieService } from 'ngx-cookie-service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
+  providers: [ CookieService,
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
